bc8a46b937000e480b80a7a935c2a074ae1ecca2
Dennis Dunne <dunned024@gmail.com>
2023-05-09 09:35:46 -0400

    add CDN to infra

--- infra/lib/StorageStack.ts
+++ infra/lib/StorageStack.ts
@@ -1,19 +1,32 @@
-import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Bucket, BlockPublicAccess, BucketEncryption } from 'aws-cdk-lib/aws-s3';
+import { CachePolicy, Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StorageStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'HoldMyClipsS3Bucket', {
+    const bucket = new Bucket(this, 'HoldMyClipsS3Bucket', {
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       bucketName: 'hold-my-clips',
-      encryption: BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      versioned: true,
-      removalPolicy: RemovalPolicy.RETAIN,
+    });
+    
+    const originAccessIdentity = new OriginAccessIdentity(this, 'HoldMyClipsOAI');
+
+    const cachePolicy = new CachePolicy(this, 'HoldMyClipsDistributionCachePolicy', {
+      defaultTtl: Duration.days(7),
+      maxTtl: Duration.days(30),
+      minTtl: Duration.days(1),
+    })
+    
+    new Distribution(this, 'HoldMyClipsDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {originAccessIdentity}),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy
+      },
     });
   }
 }
