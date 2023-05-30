f437971014766eefebd3329ce20f91990ed2d817
Dennis Dunne <dunned024@gmail.com>
2023-05-30 07:25:34 -0400

    we do a little style while deploying

--- client/src/upload/Uploader.tsx
+++ client/src/upload/Uploader.tsx
@@ -17,20 +17,24 @@ import './Uploader.css';
       // TODO: create a lambda that uploads the function
       // https://aws.amazon.com/blogs/compute/patterns-for-building-an-api-to-upload-files-to-amazon-s3/
       // "The CloudFront event is set Viewer request to meaning the function is invoked in reaction to PUT events from the client"
-      console.log(source)
-      formData.append('file', source)
-      // const videoRes = await fetch(`https://clips.dunned024.com/clips/${id}/${id}.mp4`, {
-      //   method: 'PUT',
-      //   body: source
-      // });
-      // console.log(videoRes)
-
-      // TODO: This doesn't work
-      const res = await fetch(`https://clips.dunned024.com/upload`, {
-        method: 'POST',
+      
+      const id = formData.get('id')
+      const res = await fetch(`/upload`, {
+        method: 'PUT',
         body: formData,
       });
       console.log(res)
+
+      console.log(source)
+      const videoRes = await fetch(`/clips/${id}/${id}.mp4`, {
+        headers: {
+          'Content-Type': 'video/mp4' 
+        },
+        method: 'PUT',
+        body: source
+      });
+      console.log(videoRes)
+
     } catch (error) {
       console.log(error);
     }
