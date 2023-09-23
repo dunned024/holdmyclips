eb8bf3d72e7d2fddf4db4b2cf1f4d4fe79f7d939
Dennis Dunne <dunned024@gmail.com>
2023-09-23 09:59:49 -0400

    left-pad seconds when formatting

--- client/src/services/time.ts
+++ client/src/services/time.ts
@@ -3,7 +3,9 @@ export function formatTime(value: number) {
 }
 
 export function secondsToMMSS(value: number) {
-  const secondsPastMinute = Math.floor(value % 60);
+  const secondsPastMinute = Math.floor(value % 60)
+    .toString()
+    .padStart(2, '0');
   const minutes = Math.floor(value / 60)
     .toString()
     .padStart(2, '0');
